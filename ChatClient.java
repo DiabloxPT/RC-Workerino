ae37bb5b8f56b17bc99a593ead9cd2e7d7c11cf1
Yisgramiir <dbogas738@gmail.com>
2016-12-06 14:20:35 +0000

    added new files and started client side ops

---
+++ ChatClient.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class ChatClient {
+
+    // Variáveis relacionadas com a interface gráfica --- * NÃO MODIFICAR *
+    JFrame frame = new JFrame("Chat Client");
+    private JTextField chatBox = new JTextField();
+    private JTextArea chatArea = new JTextArea();
+    // --- Fim das variáveis relacionadas coma interface gráfica
+
+    // Se for necessário adicionar variáveis ao objecto ChatClient, devem
+    // ser colocadas aqui
+    
+    
+    Socket clientSocket = null; //socket do cliente
+    Printwriter tellServer = null; // printer pra escrever pro sv
+    BufferedReader fromServer = null; //msg que vem do sv
+    String command = null; //comando (/join, /nick, etc)
+    
+
+
+
+    
+    // Método a usar para acrescentar uma string à caixa de texto
+    // * NÃO MODIFICAR *
+    public void printMessage(final String message) {
+        chatArea.append(message);
+    }
+
+    
+    // Construtor
+    public ChatClient(String server, int port) throws IOException {
+
+        // Inicialização da interface gráfica --- * NÃO MODIFICAR *
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.add(chatBox);
+        frame.setLayout(new BorderLayout());
+        frame.add(panel, BorderLayout.SOUTH);
+        frame.add(new JScrollPane(chatArea), BorderLayout.CENTER);
+        frame.setSize(500, 300);
+        frame.setVisible(true);
+        chatArea.setEditable(false);
+        chatBox.setEditable(true);
+        chatBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    newMessage(chatBox.getText());
+                } catch (IOException ex) {
+                } finally {
+                   chatBox.setText("");
+                }
+            }
+        });
+        // --- Fim da inicialização da interface gráfica
+
+        // Se for necessário adicionar código de inicialização ao
+        // construtor, deve ser colocado aqui
+	
+		try{
+			clientSocket = new Socket(server,port);
+			tellServer = new Printwriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+			fromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+
+
+    }// fim do chatClient
+
+
+    // Método invocado sempre que o utilizador insere uma mensagem
+    // na caixa de entrada
+    public void newMessage(String message) throws IOException {
+        // PREENCHER AQUI com código que envia a mensagem ao servidor
+		tellServer.println(message); // imprime no sv
+		command = message;
+		String[] cmd_tokens = command.split("\\s") // espaços em branco
+		tellServer.flush();
+	}
+
+    
+    // Método principal do objecto
+    public void run() throws IOException {
+        // PREENCHER AQUI
+		String message;
+	
+	}
+    
+
+    // Instancia o ChatClient e arranca-o invocando o seu método run()
+    // * NÃO MODIFICAR *
+    public static void main(String[] args) throws IOException {
+        ChatClient client = new ChatClient(args[0], Integer.parseInt(args[1]));
+        client.run();
+    }
+
+}
